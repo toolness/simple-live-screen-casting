0c38841781ea92bdc7dae31ca812dd87aae9b21b
Atul Varma <varmaa@gmail.com>
2011-01-24 14:29:39 -0800

    Added kEnableJPEG constant.

--- ScreenCapTheoraAppDelegate.m
+++ ScreenCapTheoraAppDelegate.m
@@ -16,9 +16,10 @@
 #import "QueueController.h"
 #import "FrameReader.h"
 
-#define kEnableTheora 0
+#define kEnableTheora 1
+#define kEnableJPEG 0
 #define kNumReaderObjects 20
-#define kFPS 1
+#define kFPS 10
 #define kImageScaling 0.25
 #define kTheoraQuality 32
 #define kTheoraKeyframeGranuleShift 6
@@ -129,31 +130,33 @@ static CVReturn displayLinkCallback(CVDisplayLinkRef displayLink,
 
 		CGContextDrawImage(myContext, dest, cgImage);
 
-		CGImageRef myContextImage = CGBitmapContextCreateImage(myContext);
-		NSURL *imageURL = [NSURL URLWithString:@"file:///Users/avarma/Desktop/screencap.jpg"];
+		if (kEnableJPEG) {
+			CGImageRef myContextImage = CGBitmapContextCreateImage(myContext);
+			NSURL *imageURL = [NSURL URLWithString:@"file:///Users/avarma/Desktop/screencap.jpg"];
 
-		float compression = 0.1;
-		int orientation = 1;
-		CFStringRef myKeys[3];
-		CFTypeRef   myValues[3];
-		CFDictionaryRef myOptions = NULL;
-		myKeys[0] = kCGImagePropertyOrientation;
-		myValues[0] = CFNumberCreate(NULL, kCFNumberIntType, &orientation);
-		myKeys[1] = kCGImagePropertyHasAlpha;
-		myValues[1] = kCFBooleanTrue;
-		myKeys[2] = kCGImageDestinationLossyCompressionQuality;
-		myValues[2] = CFNumberCreate(NULL, kCFNumberFloatType, &compression);
-		myOptions = CFDictionaryCreate( NULL, (const void **)myKeys, (const void **)myValues, 3,
-									   &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-		
-		CGImageDestinationRef imageFile = CGImageDestinationCreateWithURL((CFURLRef) imageURL, kUTTypeJPEG, 1, nil);
-		CGImageDestinationAddImage(imageFile, myContextImage, myOptions);
-		CGImageDestinationFinalize(imageFile);
-		CFRelease(imageFile);
-		CFRelease(myOptions);
-		CFRelease(myValues[0]);
-		CFRelease(myValues[2]);
-		CGImageRelease(myContextImage);
+			float compression = 0.1;
+			int orientation = 1;
+			CFStringRef myKeys[3];
+			CFTypeRef   myValues[3];
+			CFDictionaryRef myOptions = NULL;
+			myKeys[0] = kCGImagePropertyOrientation;
+			myValues[0] = CFNumberCreate(NULL, kCFNumberIntType, &orientation);
+			myKeys[1] = kCGImagePropertyHasAlpha;
+			myValues[1] = kCFBooleanTrue;
+			myKeys[2] = kCGImageDestinationLossyCompressionQuality;
+			myValues[2] = CFNumberCreate(NULL, kCFNumberFloatType, &compression);
+			myOptions = CFDictionaryCreate( NULL, (const void **)myKeys, (const void **)myValues, 3,
+										   &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+			
+			CGImageDestinationRef imageFile = CGImageDestinationCreateWithURL((CFURLRef) imageURL, kUTTypeJPEG, 1, nil);
+			CGImageDestinationAddImage(imageFile, myContextImage, myOptions);
+			CGImageDestinationFinalize(imageFile);
+			CFRelease(imageFile);
+			CFRelease(myOptions);
+			CFRelease(myValues[0]);
+			CFRelease(myValues[2]);
+			CGImageRelease(myContextImage);
+		}
 		
 		CGImageRelease(cgImage);
 		CGDataProviderRelease(pixelBufferData);
