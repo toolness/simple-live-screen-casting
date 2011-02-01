0b6cd7d636a001a883ec2c98612c057435f5e192
Atul Varma <varmaa@gmail.com>
2011-02-01 15:54:05 -0800

    Removed some extraneous commented-out code.

--- buffered-stream.js
+++ buffered-stream.js
@@ -12,19 +12,14 @@ function BufferedStream(inputStream, chunks) {
   var isEnded = !inputStream.readable;
   var wasEndSent = false;
 
-  //if (chunks)
-  //  console.log("chunks is", chunks.length);
   chunks = chunks ? chunks.slice() : [];
 
   function emitChunks() {
     if (!isPaused) {
-      //console.log("EMITCHUNX", pos, chunks.length);
       while (pos < chunks.length) {
-        //console.log("DATAZ");
         self.emit('data', chunks[pos++]);
       }
       if (pos >= chunks.length && isEnded && !wasEndSent) {
-        //console.log("ENDZ");
         self.readable = false;
         wasEndSent = true;
         self.emit('end');
@@ -39,18 +34,15 @@ function BufferedStream(inputStream, chunks) {
   };
   
   self.pause = function() {
-    //console.log('PAUSE');
     isPaused = true;
   };
   
   self.resume = function() {
-    //console.log('RESUME');
     isPaused = false;
     emitChunks();
   };
 
   self.destroy = function() {
-    //console.log('DESTROY');
     pos = Infinity;
     isEnded = true;
     wasEndSent = true;
