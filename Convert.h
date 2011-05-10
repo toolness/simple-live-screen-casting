5c8ac32afcfa78924cd7b9d56ac3e346c07a1b27
Atul Varma <varmaa@gmail.com>
2011-05-10 14:56:33 -0700

    Cleaned out everything.

--- Convert.h
+++
@@ -1,8 +0,0 @@
-// This file was originally taken from the Mozilla Rainbow project:
-// https://github.com/mozilla/rainbow
-
-/* Converter functions from libvidcap */
-int RGB32toI420(int width, int height, const char *src, char *dst);
-int BGR32toI420(int width, int height, const char *src, char *dst);
-int I420toRGB32(int width, int height, const char *src, char *dst);
-
